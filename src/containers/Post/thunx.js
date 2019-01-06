6b75133425de9469e69783dc78ddacf46ee7ebee
Anton Loyko <anton.loyko@technorely.com>
2019-01-06 16:13:24 +0200

    Update: added lib - moment and  minor change  rm then (ref https://trello.com/c/Rd43y4mK/15-front-test-commit)

--- src/containers/Post/thunx.js
+++ src/containers/Post/thunx.js
@@ -7,18 +7,6 @@ import { getPostRequest, getPostError, getPostSuccess } from './actions';
 import { API } from '../../config/api';
 import toastr from '../../helpers/toastr';
 
-// export const fetchPost = () => dispatch => {
-//   dispatch(getPostRequest());
-//   return axios // TODO --- test using generators
-//     .get(`${API.URL}/${API.POSTS}`)
-//     .then(({ data }) => {
-//       return dispatch(getPostSuccess(data.payload));
-//     })
-//     .catch(error => {
-//       toastr('error', error.message);
-//       return dispatch(getPostError(error));
-//     });
-// };
 export const fetchPost = () => async dispatch => {
   await dispatch(getPostRequest());
   try {
