8f2d6310553b909fd90ea7b5284621eb2021f9c9
George <george_tr@quantstamp.com>
2019-04-20 12:16:41 +0300

    New: Add text fields and base validation

--- src/app/containers/AdminPanelEditor/component.js
+++ src/app/containers/AdminPanelEditor/component.js
@@ -3,48 +3,108 @@
  */
 
 import React, { useState } from 'react';
-import { Value } from 'slate';
+// import { Value } from 'slate';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
 import Tooltip from '@material-ui/core/Tooltip';
 import AddIcon from '@material-ui/icons/CloudUpload';
+import TextField from '@material-ui/core/TextField';
 import Loader from '../../components/Loader';
 import Editor from '../../elements/EditorS';
 import EditorImageWrapper from '../EditorImageWrapper';
 
+import toastr from '../../../helpers/toastr';
 import styles from './style.module.css';
 
-// TODO --- remove mock initialValue
-const initialValue = Value.fromJSON({
-  document: {
-    nodes: [
-      {
-        object: 'block',
-        type: 'paragraph',
-        nodes: [
-          {
-            object: 'text',
-            leaves: [
-              {
-                text: 'A line of text in a paragraph.',
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-});
-
 const AdminPanelEditor = ({ loading, setPost }) => {
-  const [postData, updatePostData] = useState('');
-  const handlerEditor = data => updatePostData(data.toJSON());
-  const handleSetPost = () => setPost(postData);
+  const [editor, setEditor] = useState('');
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [isDescriptionError, setDescriptionError] = useState(false);
+  const [tags, setTags] = useState('');
+
+  const handlerInput = ({ target }) => {
+    switch (target.name) {
+      case 'title':
+        setTitle(target.value);
+        break;
+
+      case 'description':
+        setDescription(target.value);
+        setDescriptionError(false);
+        break;
+
+      case 'tags':
+        setTags(target.value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const handlerEditor = data => setEditor(data.toJSON());
+  const handleSetPost = () => {
+    if (
+      !title.length ||
+      !description.length ||
+      !editor.document.nodes[0].nodes[0].leaves[0].text.length
+    ) {
+      return toastr('error', 'Проверьте все обязательные поля поля');
+    }
+    if (description.length > 150) {
+      setDescriptionError(true);
+      return toastr('error', 'Превышен лимит описания');
+    }
+    const postData = {
+      title,
+      description,
+      tags,
+      editor,
+    };
+
+    setPost(postData);
+  };
 
   return (
     <div className={styles.editorWrap}>
+      <TextField
+        autoComplete="off"
+        onChange={handlerInput}
+        className={styles.textField}
+        label="Заголовок"
+        placeholder="Двенадцать дней и семь ночей..."
+        margin="normal"
+        name="title"
+        value={title}
+        required
+      />
+      <TextField
+        autoComplete="off"
+        onChange={handlerInput}
+        className={styles.textField}
+        label={`Описание: ${
+          description.length ? Math.abs(description.length - 150) : ''
+        }`}
+        placeholder="Максимум 150 символов"
+        margin="normal"
+        name="description"
+        value={description}
+        required
+        error={isDescriptionError}
+      />
+      <TextField
+        autoComplete="off"
+        onChange={handlerInput}
+        className={styles.textField}
+        label="Тэги"
+        placeholder="Тэги вводить через запятую."
+        margin="normal"
+        name="tags"
+        value={tags}
+      />
       <Editor
-        value={initialValue}
+        // value={initialValue}
         callback={handlerEditor}
         placeholder="Type your story..."
         ImgWrapper={EditorImageWrapper}
